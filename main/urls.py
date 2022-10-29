ac188fa44923ad2d0ee20ed5597524928ac803c5
nadia <67629937+nadiaverdha@users.noreply.github.com>
2022-10-29 15:27:31 +0200

    Small changes

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews import views as reviewViews
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", reviewViews.home)]
