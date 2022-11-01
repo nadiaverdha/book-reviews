f06cbe76bb59b7e6256aa21bacad7dec36774bae
nadia <67629937+nadiaverdha@users.noreply.github.com>
2022-11-01 22:19:47 +0100

    Added login and registration

--- main/urls.py
+++ main/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("accounts/", include("registration.backends.default.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
